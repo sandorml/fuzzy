5aac67d181649b173c4ee8043c464aacf2af349c
Sandor Martin Leyva <s.martin@estudiantes.matcom.uh.cu>
2019-05-13 22:10:32 -0400

    se hizo la union de  los resultados en la implicaciones

--- aggregation_methods.py
+++ aggregation_methods.py
@@ -3,29 +3,28 @@ import numpy as np
 
 
 class AggregationMethods:
-    def __init__(self, implications: list, membership_function: dict, x: tuple):
+    def __init__(self, implications: list, membership_function: dict, domain: tuple):
         self.membership_function = membership_function
         self.implication = implications
-        self.x = x
+        self.domain = domain
 
-    #TODO: hay que hacerla union de todos las listas de las x y las y
     def implications(self, results: list):
         values_x = []
         values_y = []
         for i in range(len(results)):
-            values__x = []
-            values__y = []
+
             implication = self.implication[i]
-            for x in np.arange(self.x[0], self.x[1], 0.01):
+            for x in np.arange(self.domain[0], self.domain[1], 0.01):
                 y = self.membership_function[implication](x)
-                if results[i] >= y and y >= 0:
-                    values__x.append(x)
-                    values__y.append(y)
+                vy = y if results[i] >= y and y >= 0 else results[i]
+
+                if not x in values_x:
+                    values_x.append(x)
+                    values_y.append(vy)
                 else:
-                    values__x.append(x)
-                    values__y.append(results[i])
-            values_x.append(values__x)
-            values_y.append(values__y)
+                    i_x = values_x.index(x)
+                    values_y[i_x] = values_y[i_x] if values_y[i_x] > vy else vy
+           
         return values_x, values_y
 
     def defuzzification(self, x: list, y: list, type='c'):
@@ -37,16 +36,15 @@ class AggregationMethods:
                 mp -> maximo mas pequenno
                 mg -> maximo mas grande
         '''
-        if type=='b':
-            return self.biseccion(x,y)         
-        elif type=='mc':
-            return self.middle_of_maximum(x,y)
-        elif type=='mp':
-            return self.smallest_of_maximum(x,y)
-        elif type=='mg':
-            return self.largest_of_maximum(x,y)
-        
-        return self.centroide(x,y)
+        if type == 'b':
+            return AggregationMethods.biseccion(x, y)
+        elif type == 'mc':
+            return AggregationMethods.middle_of_maximum(x, y)
+        elif type == 'mp':
+            return AggregationMethods.smallest_of_maximum(x, y)
+        elif type == 'mg':
+            return AggregationMethods.largest_of_maximum(x, y)
+        return AggregationMethods.centroide(x, y)
 
     @staticmethod
     def centroide(x: list, y: list):
@@ -56,13 +54,13 @@ class AggregationMethods:
             num += x[i]*y[i]
             den += y[i]
         return num/den
-    
+
     @staticmethod
     def biseccion(x: list, y: list):
         pass
 
     @staticmethod
-    def  middle_of_maximum(x: list, y: list):
+    def middle_of_maximum(x: list, y: list):
         max_l = max(y)
         values = []
         for i in range(len(y)):
@@ -71,19 +69,19 @@ class AggregationMethods:
         return np.average(values)
 
     @staticmethod
-    def  smallest_of_maximum(x: list, y: list):
+    def smallest_of_maximum(x: list, y: list):
         return x[y.index(max(y))]
 
     @staticmethod
-    def  largest_of_maximum(x: list, y: list):
+    def largest_of_maximum(x: list, y: list):
         y.reverse()
-        return AggregationMethods.smallest_of_maximum(x,y)
-    
+        x.reverse()
+        return AggregationMethods.smallest_of_maximum(x, y)
 
 
 class Mamdani(AggregationMethods):
-    def __init__(self, rules: list, implications: list, membership_function: dict, x: tuple):
-        AggregationMethods.__init__(self, implications, membership_function, x)
+    def __init__(self, rules: list, implications: list, membership_function: dict, domain: tuple):
+        AggregationMethods.__init__(self, implications, membership_function, domain)
         self.rules = [Rule(item.split(), max, min) for item in rules]
 
     def evaluate(self, values: dict):
