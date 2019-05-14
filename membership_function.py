09f5c650203e1dd92de380335519c30ff50a39fc
Sandor Martin Leyva <s.martin@estudiantes.matcom.uh.cu>
2019-05-13 22:10:02 -0400

    se arreglo la funcionde trapecio

--- membership_function.py
+++ membership_function.py
@@ -5,20 +5,32 @@ def currying(f):
         return lambda *args2: curried(*(args+args2))
     return curried
 
+
 @currying
-def triangular(a, b, m,x):
+def triangular(a, b, m, x):
     if x <= a or x >= b:
         return 0
     elif a < x <= m:
         return (x-a)/(m-a)
     return (b-x)/(b-m)
 
+
 @currying
-def pi(a, b, c, d,x):
-    if x <= a or x >= d:
-        return 0
-    elif a< x<= b:
-        return (x-a)/(b-a)
-    return (d-x)/(b-c)
+def pi(a, b, c, d, x):
+    if a <= x <= b:
+        return (x - a) / (b - a)
+    if c <= x <= d:
+        return (d - x) / (d - c)
+    return 1 if b <= x <= c else 0
 
 
+# trapecio = pi(1,2,3,5)
+
+# import numpy as np
+# import matplotlib.pyplot as pl
+
+# x = np.arange(0,10,0.01)
+# y = [trapecio(i) for i in x]
+
+# pl.plot(x,y)
+# pl.show()
